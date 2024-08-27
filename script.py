185d3f5e1f3c3a505914d32eaf2c9847a660f30a
NicolasAmadori <nicolas.amadori02@gmail.com>
2024-08-27 17:15:37 +0200

    generated clean documents

--- script.py
+++ script.py
@@ -23,6 +23,8 @@ from llm import LLMGraphTransformer
 
 from langchain_core.prompts import PromptTemplate
 
+from neo4j.debug import watch
+
 def getHuggingFaceModel(model_id, hf_token):
     # model_id = "microsoft/Phi-3-mini-128k-instruct"
     # model_id = "microsoft/Phi-3-mini-4k-instruct"
@@ -42,7 +44,7 @@ def getHuggingFaceModel(model_id, hf_token):
                     max_new_tokens=1024,
                     top_k=50,
                     temperature=0.1,
-                    return_full_text = False)
+                    return_full_text = True)
     llm = HuggingFacePipeline(pipeline=pipe,
                             pipeline_kwargs={"return_full_text": False}) # <----- IMPORTANT !!!
     return llm
@@ -65,7 +67,8 @@ def getDocuments(path):
             documents.append(Document(
                 page_content=content,
                 metadata={"path": file_path,
-                        "title": content.split("\n")[2]}))
+                        "title": content.split("\n")[2],
+                        "file_name": file}))
     return documents
 
 def generateGraph(documents):
@@ -107,33 +110,59 @@ def cleanMarkup(llm, text):
 
     return chain.invoke({"text": text})   
 
-def cleanDocuments(llm, documents):
-    # output_directory = "workspace/cleaned_documents"
-    # os.makedirs(output_directory, exist_ok=True)
+def cleanDocuments(llm, documents, create_files=False):
+    if create_files:
+        output_directory = "workspace/cleaned_documents"
+        os.makedirs(output_directory, exist_ok=True)
     i = 0
     for d in documents:
         i+=1
         print(f"Cleaning the document number: {i}/{len(documents)}")
         d.page_content = cleanMarkup(llm, d.page_content)
-        # output_path = os.path.join(output_directory, f"{d.metadata['title']}.txt")
-        # with open(output_path, "w") as file:
-        #     file.write(cleaned_text)
+        if create_files:
+            output_path = os.path.join(output_directory, d.metadata['file_name'])
+            with open(output_path, "w") as file:
+                file.write(d.page_content)
     return documents
 
-if __name__ == '__main__':
+def test_neo4j_connection(graph):
+    try:
+        result = graph.query("RETURN 'Connection successful' AS message")
+        print(result)
+        return True
+    except Exception as e:
+        print(f"Errore durante la verifica della connessione al db Neo4j: {e}")
+        return False
+
+def save_graph_to_file(generated_graph):
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     
-    from neo4j.debug import watch
+    file_name = f"generated_graph_{current_time}.txt"
+    
+    try:
+        with open(file_name, 'w', encoding='utf-8') as f:
+            for graph_document in generated_graph:
+                f.write(str(graph_document) + '\n')
+        print(f"Graph successfully saved to {file_name}")
+    except Exception as e:
+        print(f"Error while saving the graph to file: {e}")
+
+if __name__ == '__main__':
     watch("neo4j")
 
-    HF_TOKEN = "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"
-    # os.environ["NEO4J_URI"]="neo4j+s://6743dae3.databases.neo4j.io"
-    # os.environ["NEO4J_USERNAME"]="neo4j"
-    # os.environ["NEO4J_PASSWORD"]="gUxckYtBrObmia7f1ByzOp_0H4GPlW6-wZha7TofvEI"
-    # os.environ["AURA_INSTANCEID"]="6743dae3"
-    # os.environ["AURA_INSTANCENAME"]="Instance01"
+    torch.cuda.empty_cache()
+    gc.collect()
 
-    graph = Neo4jGraph(url= "neo4j+s://6743dae3.databases.neo4j.io", username="neo4j", password="gUxckYtBrObmia7f1ByzOp_0H4GPlW6-wZha7TofvEI")
+    HF_TOKEN = "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"
+    os.environ["NEO4J_URI"]="neo4j+s://bbef2ff2.databases.neo4j.io"
+    os.environ["NEO4J_USERNAME"]="neo4j"
+    os.environ["NEO4J_PASSWORD"]="fdZslu0qGuZhCiR9pasipKRR-iLDgz9AMp8KVS9Uf2s"
+    os.environ["AURA_INSTANCEID"]="bbef2ff2"
+    os.environ["AURA_INSTANCENAME"]="Instance01"
+
+    graph = Neo4jGraph(url= "neo4j+s://bbef2ff2.databases.neo4j.io", username="neo4j", password="fdZslu0qGuZhCiR9pasipKRR-iLDgz9AMp8KVS9Uf2s")
     print("\n1. Neo4j Graph Created.\n")
+    print(f"Risultato test: {test_neo4j_connection(graph)}")
 
     llm = getHuggingFaceModel(model_id="meta-llama/Meta-Llama-3-8B-Instruct", hf_token = HF_TOKEN)
     print("\n2. Model downloaded.\n")
@@ -144,15 +173,16 @@ if __name__ == '__main__':
     documents = getDocuments("/workspace/crawl")
     print(f"\n4. {len(documents)} Documents read.\n")
 
-    documents = cleanDocuments(llm, documents)
+    cleaned_documents = cleanDocuments(llm, documents, create_files=True)
     print(f"\n5. Documents cleaned.\n")
     
-    generated_graph = generateGraph(documents[:])
-    print("\n6. Graph Generated.\n")
+    # generated_graph = generateGraph(cleaned_documents)
+    # print("\n6. Graph Generated.\n")
 
-    try:
-        graph.add_graph_documents(generated_graph)
-    except Exception as e:
-        print(f"Errore durante l'aggiunta del grafo al db: {e}")
-    else:
-        print("\n7. Graph added to neo4j db.\n")
+    # save_graph_to_file(generated_graph)
+    # try:
+    #     graph.add_graph_documents(generated_graph)
+    # except Exception as e:
+    #     print(f"Errore durante l'aggiunta del grafo al db: {e}")
+    # else:
+    #     print("\n7. Graph added to neo4j db.\n")
